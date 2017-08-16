c9787baac6132e7a4283c313baba8fd924202b28
kieranmcmanus <kmcmanus41092@gmail.com>
2017-08-16 18:55:16 -0400

    8/17/17

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// Make it rain!
+
+//When the user opens the 'About' page
+  //Image fades in
+//When the user scrolls down
+  //Info text fades in
+
+//On the 'Releases' page
+//When the user clicks on 'Solo'
+  //Shattuck releases slide out
+  //Solo releases slide in
+//When the user clicks on 'Shattuck'
+  //Solo releases slide out
+  //Shattuck releases slide in
+
+//On the 'About' page
+//When the user clicks 'next'
+  //image slides out and next image slides in
+//When the user clicks 'previous'
+  //image slides out and previous image slides in
+//If user is on last image and clicks 'next'
+  //load first image
+//If the user is on first image and clicks previous
+  //load last image
