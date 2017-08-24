7d37a30a03aeb4185c5c111a964d501ad3b33849
kieranmcmanus <kmcmanus41092@gmail.com>
2017-08-23 21:43:56 -0400

    8/23/17

--- js/main.js
+++ js/main.js
@@ -9,18 +9,71 @@ var path = location.pathname;
 console.log(location.pathname);
 
 if(path = "about.html") {
-  $('img').fadeIn(2000);
-  console.log('Image faded in!');
+  $('#first').fadeIn(2000);
+  $('p').fadeIn(2000);
+  $('.venues').fadeIn(2000);
+  //
+  // $(window).on('scroll', function () {
+  //   var distanceScrolled = $(window).scrollTop();
+  //   console.log('The distance scrolled is: ' + distanceScrolled);
+  //
+  //   if (distanceScrolled > 200) {
+  //     $('p').fadeIn(1000);
+  //     $('.venues').fadeIn(1000);
+  //   }
+  // });
 
-  $(window).on('scroll', function () {
-    var distanceScrolled = $(window).scrollTop();
-    console.log('The distance scrolled is: ' + distanceScrolled);
+  //On the 'About' page
+  //When the user clicks 'next'
+    //image slides out and next image slides in
+  //When the user clicks 'previous'
+    //image slides out and previous image slides in
+  //If user is on last image and clicks 'next'
+    //load first image
+  //If the user is on first image and clicks previous
+    //load last image
 
-    if (distanceScrolled > 200) {
-      $('p').fadeIn(1000);
-      $('.venues').fadeIn(1000);
-    }
-  });
+    var currentImageNumber = 0;
+    var numberOfImages = $('img').length - 1;
+    console.log("There are " + numberOfImages + "images");
+    // When the user clicks the next button (#next)
+    $('#next').on('click', function () {
+      // if currentImageNumber is less than 3
+        // Add one to currentImageNumber
+        // Log currentImageNumber to the console
+     // else
+       // Set currentImageNumber to zero
+       // Log currentImageNumber to the console
+       if (currentImageNumber < numberOfImages) {
+         currentImageNumber += 1;
+         //console.log(currentImageNumber);
+       } else {
+         currentImageNumber = 0;
+         //console.log(currentImageNumber);
+       }
+       $('img').fadeOut(0);
+       $('img').eq(currentImageNumber).fadeIn(0);
+    });
+
+    // When the user clicks the next button (#next)
+    $('#previous').on('click', function () {
+      // if currentImageNumber is greater than 0
+        // Subtract one from currentImageNumber
+        // Log currentImageNumber to the console
+      // else
+       // Set currentImageNumber to three
+       // Log currentImageNumber to the console
+       if (currentImageNumber > 0) {
+         currentImageNumber -= 1;
+         //console.log(currentImageNumber);
+       } else {
+         currentImageNumber = numberOfImages;
+         //console.log(currentImageNumber);
+       }
+       $('img').fadeOut(0);
+       $('img').eq(currentImageNumber).fadeIn(0);
+    });
+
 }
 $('.soloReleases').addClass('selected');
 
@@ -48,13 +101,3 @@ $('.shattuckReleases').on('click', function() {
 //When the user clicks on 'Shattuck'
   //Solo releases slide out
   //Shattuck releases slide in
-
-//On the 'About' page
-//When the user clicks 'next'
-  //image slides out and next image slides in
-//When the user clicks 'previous'
-  //image slides out and previous image slides in
-//If user is on last image and clicks 'next'
-  //load first image
-//If the user is on first image and clicks previous
-  //load last image
